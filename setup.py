0ee55b84596a8decc0d03fc1307e58f2f692e404
tommy2tone <thomas.hildebrand1@gmail.com>
2020-10-09 20:47:17 -0400

    Add setup.py to help others initiate the program.

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+requires = [
+    'tkinter',
+    'sqlite3',
+]
+
+setup(
+    name='Bookstore',
+    version='',
+    packages=find_packages(),
+    keywords='python tkinter sqlite',
+    url='',
+    license='',
+    author='Tom Hildebrand',
+    author_email='t1manster@gmail.com',
+    description='Bookstore application with GUI (TKinter) and database (SQLite) interaction.',
+    install_requires=requires
+)
